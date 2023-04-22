e498026ef4443d858e4fc09db599feb38cac6332
kodruck <jonathan.salazar@ucrso.info>
2023-04-22 10:11:35 -0600

    JON-97126 WIP-Contents views

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,7 @@
 class Content < ApplicationRecord
+    #Es en la manera en que se acegura que sea requerido
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :start_date, presence: true
+    validates :end_date,presence: true
 end
