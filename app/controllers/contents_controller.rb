ed370ee4a61873030e6076074c477822484fd6ec
FabricioVega <fabricio.vegaalfaro@ucr.ac.cr>
2023-04-22 18:07:28 +0200

    FVA-c18377 WIP-Contents views

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,25 +1,18 @@
 class ContentsController < ApplicationController
   before_action :set_content, only: %i[ show edit update destroy ]
 
-  # GET /contents or /contents.json
   def index
     @contents = Content.all
   end
 
-  # GET /contents/1 or /contents/1.json
-  def show
-  end
+  def show; end
 
-  # GET /contents/new
   def new
     @content = Content.new
   end
 
-  # GET /contents/1/edit
-  def edit
-  end
+  def edit; end
 
-  # POST /contents or /contents.json
   def create
     @content = Content.new(content_params)
 
@@ -34,7 +27,6 @@ class ContentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /contents/1 or /contents/1.json
   def update
     respond_to do |format|
       if @content.update(content_params)
@@ -47,7 +39,6 @@ class ContentsController < ApplicationController
     end
   end
 
-  # DELETE /contents/1 or /contents/1.json
   def destroy
     @content.destroy
 
@@ -58,12 +49,10 @@ class ContentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_content
       @content = Content.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def content_params
       params.require(:content).permit(:title, :description, :start_date, :end_date)
     end
--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Content, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject {
+    described_class.new(title: "Anything",
+                        description: "Lorem ipsum",
+                        start_date: DateTime.now,
+                        end_date: DateTime.now + 1.week)
+  }
+
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without title" do
+    subject.title = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without description" do
+    subject.description = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without start_date" do
+    subject.start_date = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without end-date" do
+    subject.end_date = nil
+    expect(subject).to_not be_valid
+  end
+  
 end
