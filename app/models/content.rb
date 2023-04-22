5d1e6892c5f7207b9e3807a27cba4fd0974f496f
AlexaVegaRojas <alexavr92@gmail.com>
2023-04-22 10:07:49 -0600

    AVR-C09317 WIP-Contents vi

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,2 +1,10 @@
 class Content < ApplicationRecord
+
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :start_date, presence: true
+    validates :end_date, presence: true
+
 end
+
+# title, description,start_date estan en migrate 
