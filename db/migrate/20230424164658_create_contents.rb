f1920771c330863d98327796407bf4f7efe82599
Raquel <raquelarias2010@hotmail.com>
2023-04-24 17:37:32 -0600

    RAM-C00708 Created content s structure

---
+++ db/migrate/20230424164658_create_contents.rb
@@ -0,0 +1,12 @@
+class CreateContents < ActiveRecord::Migration[7.0]
+  def change
+    create_table :contents do |t|
+      t.string :tittle
+      t.text :description
+      t.datetime :start_date
+      t.datetime :end_date
+
+      t.timestamps
+    end
+  end
+end
