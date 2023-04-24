5784741831a642296ed1b63086f4256289ae7b9d
JoshueMora <joshue@DESKTOP-09UTHSK.localdomain>
2023-04-23 18:37:32 -0600

    JMR-C15152 WIP-Contents views

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,10 @@ module BaseProject2023
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{yml}')]
+    I18n.default_locale = :es
+
   end
 end
