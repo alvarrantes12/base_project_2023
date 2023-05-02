40325cd707031b94dd7669ec61569d0fe813806c
davidrorez <davidrorez@gmail.com>
2023-05-01 18:35:49 -0600

    DRR-C06889 Done contents

---
+++ app/helpers/contents_helper.rb
@@ -0,0 +1,8 @@
+module ContentsHelper
+    def special_types
+      Content.special_types.keys.map do |type|
+        [t("activerecord.attributes.content.types.#{type}"), type]
+      end
+    end
+  end
+  
