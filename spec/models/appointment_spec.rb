2adfa3cd8f57543b92d1b4d94936a2d0076b34ee
BCHDavid <hidalgodavid850@gmail.com>
2023-05-02 18:36:50 -0600

    DHA-B73808 correciones

--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # describe "associations" do
+  #   it { should belong_to(:doctor) }
+  #   it { should belong_to(:patient) }
+  # end
+  subject {
+    described_class.new(date: DateTime.now,
+                        doctor: Doctor.create!(first_name: "Doc1", last_name: "Doctor", code: "12345"),
+                        patient: Patient.create!(first_name: "Pat1", last_name: "Patient"))
+  }
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
 end
