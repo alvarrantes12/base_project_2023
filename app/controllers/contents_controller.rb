f77ac4820f3667392f02b68766c82d1d59551bf3
AntonyB2002 <abarrantes211@gmail.com>
2023-05-03 17:22:04 -0600

    feature/ABB-C10922 Done contents

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -1,26 +1,20 @@
 class ContentsController < ApplicationController
   before_action :set_content, only: %i[ show edit update destroy ]
 
-  # GET /contents or /contents.json
   def index
-    binding.pry
     @contents = Content.all
   end
 
-  # GET /contents/1 or /contents/1.json
   def show
   end
 
-  # GET /contents/new
   def new
     @content = Content.new
   end
 
-  # GET /contents/1/edit
   def edit
   end
 
-  # POST /contents or /contents.json
   def create
     @content = Content.new(content_params)
 
@@ -33,7 +27,6 @@ class ContentsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /contents/1 or /contents/1.json
   def update
     respond_to do |format|
       if @content.update(content_params)
@@ -44,7 +37,6 @@ class ContentsController < ApplicationController
     end
   end
 
-  # DELETE /contents/1 or /contents/1.json
   def destroy
     @content.destroy
 
@@ -54,13 +46,11 @@ class ContentsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_content
       @content = Content.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def content_params
-      params.require(:content).permit(:title, :description, :start_date, :end_date)
+      params.require(:content).permit(:title, :description, :start_date, :end_date, :special_type)
     end
 end
