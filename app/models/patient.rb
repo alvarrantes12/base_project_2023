12e093d187af27c707acc1c749cc151e0477a97f
JoseIsaacLazo <jose.lazo@ucrso.info>
2023-05-04 01:04:26 -0600

    JILL-C04168 modification of the indexes, deletion of comments and files that are not used, unit tests

---
+++ app/models/patient.rb
@@ -0,0 +1,7 @@
+class Patient < ApplicationRecord
+    has_many :appointments
+    has_many :doctors, through: :appointments
+
+    validates :first_name, presence: true, length: { maximum: 8}
+    validates :last_name, presence: true, length: {minimum: 3}
+end
