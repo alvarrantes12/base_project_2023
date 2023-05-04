1dcd3340077dcf9243d469ca3c5ed87f4cabc613
AntonyB2002 <abarrantes211@gmail.com>
2023-05-03 22:35:41 -0600

    feature/ABB-C10922 Appointment, Doctor and Patient

--- app/helpers/contents_helper.rb
+++ app/helpers/contents_helper.rb
@@ -2,7 +2,7 @@ module ContentsHelper
 
   def special_types
     Content.special_types.keys.map do |type|
-      [t("activerecord.attributes.content.types.#{type}")]
+      [t("activerecord.attributes.content.types.#{type}"), type]
     end
   end
 end
--- db/migrate/20230502231614_add_type_contents.rb
+++ db/migrate/20230502231614_add_type_contents.rb
@@ -1,5 +1,5 @@
 class AddTypeContents < ActiveRecord::Migration[7.0]
   def change
-      add_column :contents, :special_type, :integer
+    add_column :contents, :special_type, :integer
   end
 end
