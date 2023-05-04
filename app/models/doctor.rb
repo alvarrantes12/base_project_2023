bf7d85a36f463e80a53cc5c20d814a7b3db63ab0
KendallC09 <kendallconejo00@gmail.com>
2023-05-03 18:08:41 -0600

    KCA-C02310 WIP doctor, patient and appointment views

---
+++ app/models/doctor.rb
@@ -0,0 +1,8 @@
+class Doctor < ApplicationRecord
+  has_many :appointments
+  has_many :patients, through: :appointments
+
+  validates :first_name, presence: true, length: { maximum: 8 }
+  validates :last_name, presence: true, length: { minimum: 3 }
+  validates :code, presence: true, uniqueness: true
+end
