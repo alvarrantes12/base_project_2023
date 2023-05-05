65f3e6320bde708518794eb59d5163fc255c7e14
davidrorez <davidrorez@gmail.com>
2023-05-04 21:11:57 -0600

    DRR-C06889 Done Appointment, Doctor and Patient

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Doctor.create!(first_name: "Doc1", last_name: "Doctor", code: "123")
+Doctor.create!(first_name: "Doc2", last_name: "Doctor", code: "345")
+
+Patient.create!(first_name: "Pat1", last_name: "Patient")
+Patient.create!(first_name: "Pat2", last_name: "Patient")
---
+++ spec/models/doctor_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Doctor, type: :model do
+  subject {
+    described_class.new(first_name: "TestF",
+                        last_name: "Tes",
+                        code: "8386645")
+  }
+ it "is valid with valid attributes" do
+  expect(subject).to be_valid
+ end
+
+it "is not valid without first_name" do
+  subject.first_name = nil
+  expect(subject).to_not be_valid
+end
+
+it "is not valid without last_name" do
+  subject.last_name = nil
+  expect(subject).to_not be_valid
+end
+
+it "is not valid without code" do
+  subject.code = nil
+  expect(subject).to_not be_valid
+end
+
+end
