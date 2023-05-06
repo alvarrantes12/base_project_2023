4d65c266d4ba3fe4ac13a9c1e0074f9c57db72df
AlexaVegaRojas <alexavr92@gmail.com>
2023-05-05 23:33:15 -0600

    AVR-C09317 tarea1

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,11 +1,11 @@
 class Content < ApplicationRecord
 
-    validates :title, presence: true
-    validates :description, presence: true
-    validates :start_date, presence: true
-    validates :end_date, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
     
-    enum special_type: {'video': 0, 'streaming': 1, 'image': 2}
+  enum special_type: {'video': 0, 'streaming': 1, 'image': 2}
 end
 
  
--- spec/models/appointment_spec.rb
+++ spec/models/appointment_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Appointment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+ subject{
+  described_class.new( date: DateTime.now,
+                      doctor_id:"123",
+                      patient_id: "345")
+ }
+  
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid
+  end
+
+  it "is not valid without date" do
+   subject.date = nil
+   expect(subject).to_not be_valid
+  end
+
+  it "is not valid without doctor_id" do
+    subject.doctor_id = nil
+    expect(subject).to_not be_valid
+  end
+
+  it "is not valid without patient_id" do
+    subject.patient_id = nil
+    expect(subject).to_not be_valid
+  end
 end
