e41b90cefbd3d53e0eef00e1eebc825dc48779da
fabriciof24 <fabricio.fonseca@ucrso.info>
2023-05-08 15:44:50 -0600

    ffs-b83033 Done contents

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :patients
+  resources :appointments
+  resources :doctors
   resources :contents
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
---
+++ db/migrate/20230508204112_create_appointments.rb
@@ -0,0 +1,11 @@
+class CreateAppointments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :appointments do |t|
+      t.datetime :date
+      t.belongs_to: doctors
+      t.belongs_to: patient
+
+      t.timestamps
+    end
+  end
+end
